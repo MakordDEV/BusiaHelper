ba7ffde9e3e443123ce05a49100f483337306866
MakordDEV <143116333+MakordDEV@users.noreply.github.com>
2024-05-30 14:22:49 +0300

    Added a nice interface

--- Program.cs
+++ Program.cs
@@ -1,23 +1,39 @@
-﻿using SixLabors.ImageSharp;
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Webp;
 using SixLabors.ImageSharp.Processing;
 
 class Program
 {
-
     static void Main()
     {
-        string inputDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "input");
-        string outputDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "output");
+        // Starting
+        welcome();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Идет конвертация изображений:");
+        Console.ResetColor();
+
+        Console.WriteLine($"------");
+
+        string executableFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        string appDirectory = Path.GetDirectoryName(executableFilePath);
+
+        string inputDirectoryPath = Path.Combine(appDirectory, "input");
+        string outputDirectoryPath = Path.Combine(appDirectory, "output");
         bool shouldRetry = false;
 
         do
         {
             try
             {
+                // Checking directory step
                 if (!Directory.Exists(inputDirectoryPath))
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Директория с исходными изображениями не найдена.");
+                    Console.ResetColor();
                     return;
                 }
 
@@ -30,30 +46,40 @@ class Program
 
                 if (files.Length == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Нет изображений для конвертации в директории 'input'.");
+                    Console.ResetColor();
                     return;
                 }
 
+                // Count variables
                 int convertedCount = 0;
                 int skippedCount = 0;
                 int errorCount = 0;
 
-                Console.WriteLine("Идет конвертация изображений:");
-
+                // Working step
                 foreach (string inputFile in files)
                 {
                     string fileName = Path.GetFileNameWithoutExtension(inputFile);
                     string newFilePath = Path.Combine(outputDirectoryPath, fileName + ".webp");
 
+                    // Checking files
                     if (File.Exists(newFilePath))
                     {
-                        Console.WriteLine($"\nФайл '{fileName}.webp' уже существует в директории 'output' и был пропущен.");
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Файл '{fileName}.webp' уже существует в директории 'output' и был пропущен.");
+                        Console.ResetColor();
                         skippedCount++;
+                        // Sleep
+                        Thread.Sleep(610);
                         continue;
                     }
 
+                    // Convert message 
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write($"\rКонвертация {convertedCount + 1}/{files.Length}... {GetSpinnerAnimation(convertedCount)}");
 
+                    // Resize and save 
                     try
                     {
                         using (var image = Image.Load(inputFile))
@@ -64,30 +90,48 @@ class Program
                             convertedCount++;
                         }
                     }
+
+                    // Catch errors
                     catch (Exception ex)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"Произошла ошибка при конвертации файла '{fileName}': {ex.Message}");
+                        Console.ResetColor();
                         errorCount++;
                     }
+
+                    // Sleep
+                    Thread.Sleep(610);
                 }
 
-                Console.WriteLine($"\nКонвертация завершена. Успешно сконвертировано: {convertedCount}, пропущено: {skippedCount}, с ошибкой: {errorCount}");
+                // Done message
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Конвертация завершена. Успешно сконвертировано: {convertedCount}, пропущено: {skippedCount}, с ошибкой: {errorCount}");
+                Console.ResetColor();
+                Console.WriteLine($"------\n");
                 shouldRetry = true;
             }
+            // Catch errors
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                Console.ResetColor();
                 shouldRetry = true;
             }
+            // Finally step
             finally
             {
                 if (shouldRetry)
                 {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
                     Console.WriteLine("Нажмите R, чтобы выполнить повторную конвертацию. Нажмите любую другую клавишу, чтобы закрыть это окно.");
+                    Console.ResetColor();
                     ConsoleKeyInfo keyInfo = Console.ReadKey();
                     if (keyInfo.KeyChar == 'R' || keyInfo.KeyChar == 'r')
                     {
-                        Console.Write("\n");
+                        Console.Write("\n\n\n");
                         shouldRetry = true;
                     }
                     else
@@ -97,13 +141,32 @@ class Program
                 }
                 else
                 {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
                     Console.WriteLine("Нажмите любую клавишу, чтобы закрыть это окно.");
+                    Console.ResetColor();
                     Console.ReadKey();
                 }
             }
         } while (shouldRetry);
     }
 
+    // Welcome message
+    static void welcome()
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("BusiaHelper :3");
+        Console.WriteLine("(C) Busia Studios & IKR Studios 2024\n");
+
+        Console.ResetColor();
+
+        Thread.Sleep(1500);
+
+        return;
+    }
+
+    // Process animation
     static char GetSpinnerAnimation(int index)
     {
         char[] spinner = { '/', '|', '\\', '-' };
